d42b4910d522dbd47d542af27fa2af810713c9a3
rohit-jangra-dx <rjangradev@gmail.com>
2025-10-06 08:59:35 +0530

    added custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
+
+# custom manager for custom user
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+           raise ValueError("The Email field must be set.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+        
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        
+        if not password:
+            raise ValueError("Superusers must have a password.")
+        
+        return self.create_user(email, username, password, **extra_fields)
+        
+        
 # Create your models here.
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
+    objects = CustomUserManager()
+    
+    def __str__(self):
+        return self.email  
--- workspaces/models.py
+++ workspaces/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from accounts.models import CustomUser
 
 # Create your models here.
+class WorkSpace(models.Model):
+    pass
